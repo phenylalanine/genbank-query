85d3b897608f3d4988b1ae18d1b68fed2d02ea88
Ryan Larson <ryan.mango.larson@gmail.com>
2013-06-30 18:13:17 -0700

    Adding processor interface

---
+++ src/groovy/edu/pdx/cs/data/Processor.groovy
@@ -0,0 +1,11 @@
+package edu.pdx.cs.data
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ryan
+ * Date: 6/30/13
+ * Time: 3:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Processor {
+    public void process();
+}
