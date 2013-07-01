618cc9b6bd25ece397d1566f8b989c7aad3064b4
Ryan Larson <ryan.mango.larson@gmail.com>
2013-06-30 18:24:49 -0700

    This commit has a few parts.
    1: I added the Mockito mocking library to the BuildConfig.groovy. Mockito lets you make Mock objects and define their behavior for use in testing (see the test for an example).
    
    2: I implemented the processor that turns a RichSequence into an Organism domain object.
    
    3: I implemented an integration test that mocks out a RichSequence, hands it to the processor, then verifies that the processor created the correct Organism domain object in the test database.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,7 +43,8 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         compile group:'org.biojava', name: 'core', version: '1.8.+'
-        // runtime 'mysql:mysql-connector-java:5.1.22'
+
+        test group: 'org.mockito', name: 'mockito-all', version: '+'
     }
 
     plugins {
