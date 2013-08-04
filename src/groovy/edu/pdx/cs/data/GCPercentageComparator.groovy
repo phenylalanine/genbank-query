0a88700613f110ddccd51c1b39e5e54c9a55477e
JG Miller <jgm2@pdx.edu>
2013-08-04 12:50:30 -0700

    Segregated some testing of persistent data into a separate class, for easier @Ignore if necessary; other minor fixes

--- src/groovy/edu/pdx/cs/data/GCPercentageComparator.groovy
+++ src/groovy/edu/pdx/cs/data/GCPercentageComparator.groovy
@@ -7,16 +7,19 @@
 
 package edu.pdx.cs.data
 
+import java.math.MathContext
 import org.apache.commons.logging.LogFactory
 import webapp.Organism
 
 class GCPercentageComparator {
 	private static final log = LogFactory.getLog(this)
+	private int bigDecimalScale
 	private Organism orgMaster, orgCompared
 	GCPercentageComparison gcPercentageComp
 	BigDecimal gcDifference
 	
 	public GCPercentageComparator(org1, org2) {
+		bigDecimalScale = 10
 		orgMaster = org1
 		orgCompared = org2
 		compare()
@@ -32,7 +35,7 @@ class GCPercentageComparator {
 	
 	// Calculate MCUF.
 	private void compare() {
-		gcDifference = Math.abs(orgMaster.gcPercentage.toBigDecimal() - orgCompared.gcPercentage.toBigDecimal())
+		gcDifference = Math.abs(orgMaster.gcPercentage.toBigDecimal().subtract(orgCompared.gcPercentage.toBigDecimal(), MathContext.UNLIMITED))
 		gcPercentageComp = new GCPercentageComparison(orgMaster.organismId, orgMaster.scientificName, orgMaster.taxonomyId,
 			orgMaster.gcPercentage, orgCompared.organismId, orgCompared.scientificName, orgCompared.taxonomyId,
 			orgCompared.gcPercentage, gcDifference)
